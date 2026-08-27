f307438693e9728407f66135cdfa65808626dce4
Physical Geography Forever! <dntjdwo4990@snu.ac.kr>
2026-08-27 12:42:17 +0900

    Add frozen EGHM reproducibility fingerprint contract

---
+++ eghm_reproducibility_contract.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Frozen bitwise fingerprints for the deterministic Seoyeongari EGHM path.
+
+Established by Stage61d on 2026-08-27 and reproduced on 8/8 heterogeneous
+GitHub-hosted runners before being promoted to a regression contract.
+"""
+EXPECTED_FINGERPRINTS = {
+    'clean_tmean': '8e9f4c499d61d257f551ac73a0e9b6a3ee1ecc55a3cab02dc72204ebc603bf52',
+    'clean_tmin': 'c3d63da006c132870cfedabc1966b3bb7b93b5fb7d8382685ebb75998bdc5565',
+    'clean_tmax': '26265a6d0769cbf34adf8cae25299dac1b6acea8bc6ef62c32063b6e38f4811a',
+    'clean_pre': '4e46da169e1f3d7d525b33e3640f65eb145828c6e0062a73c97a9c34236142f0',
+    'clean_wind': '493d169dc7e7a32e90deacd074f49a85c1de866c4dad654943e70ea28243bc63',
+    'clean_sun': '178285509a887740957839590dfe8db7663f148f59ac6a95fa546a05c84ae3df',
+    'forcing_pre': '4e46da169e1f3d7d525b33e3640f65eb145828c6e0062a73c97a9c34236142f0',
+    'forcing_pes': '0ea37bab0ed2f0befca9ad7fb396540cfa548b4265136379c52b753caa5408c4',
+    'forcing_eto': '7757d113b1a1d836f608c0cd920e7ecd95fe5c2633c387d1e062005ab935dafc',
+    'forcing_ep': 'a2b1153cd92cf74bda7b87c80231f5e08c7aaa1440aa4e73641a8852518fee5f',
+    'forcing_pp': '448e0103b04eaf0e43a928fec88a4f129e388731d02e8861c930fca5aeca6ee3',
+    'V': 'dede5d3295413c303c662f07a774d832f3e6524592c4e5ffe4b34efaeff4fd94',
+    'area': '7c8d7dd1c5bb738cb2fa8ff89d1b06e6ede11b866b15484518ebf7a445fc934e',
+    'return_flow': '4b97f284f0ce8ee8da01559e02421ccbf04eabe0315b92aad5b246173869bb33',
+    'exposed': '355e83f77f2c7955445fd1649c38b1d5fdf25e1b9df99f1a463b662d20e9a3de',
+    'E7': 'd84e1ebf9ea5856376e14a8c1afa2dad1809fcf21d728ad665c9cf84ad21f447',
+    'state': '193e19256ab994290d455f26b84c38c671314b42c90e10d665a95f33e26a0dc8',
+    'S': '354cf1f966e9fd370e01e62ff5374821a8e49003621d002b56959c053fb2f71f',
+    'H': '4e0fc0144514a0734c7fb849b1b356855b8eb54123db725b7a1a5db86ed2a169',
+}
